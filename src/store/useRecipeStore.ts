2a1059ed6c56eab875b06a28ed15ee0ae947f544
EKallens <e.kallensp@gmail.com>
2024-12-22 22:06:02 -0300

    refactor: remove unused stores

--- src/store/useRecipeStore.ts
+++
@@ -1,7 +0,0 @@
-import { IRecipe } from '@/interfaces/recipes/recipes.interface'
-import { create } from 'zustand'
-
-export const useRecipeStore = create((set) => ({
-    recipes: [],
-    setRecipes: (recipes: IRecipe[]) => set({ recipes })
-}))
--- src/store/useTagStore.ts
+++
@@ -1,7 +0,0 @@
-import { TagStore } from '@/interfaces/tags/tags.interface'
-import { create } from 'zustand'
-
-export const useTagStore = create<TagStore>((set) => ({
-    tags: [],
-    setTags: (tags) => set({ tags })
-}))
