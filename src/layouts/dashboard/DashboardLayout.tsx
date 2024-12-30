0791f35f1288cf7972f802c14689f30d631d7095
EKallens <e.kallensp@gmail.com>
2024-12-29 22:28:10 -0300

    feat: add tag sheets

--- src/layouts/dashboard/DashboardLayout.tsx
+++ src/layouts/dashboard/DashboardLayout.tsx
@@ -8,8 +8,8 @@ import { Toaster } from 'sonner'
 export const DashboardLayout = (): JSX.Element => {
     return (
         <div className="dark:bg-boxDark2 dark:text-bodyDark">
-            {/* <EditTagSheet />
-            <CreateTagSheet /> */}
+            <EditTagSheet />
+            <CreateTagSheet />
             <div className="flex h-screen overflow-hidden">
                 <Sidebar />
                 <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
