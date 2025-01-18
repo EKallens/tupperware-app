9b90528ad4fef7fb9f9013fe42f8c1671996e706
EKallens <e.kallensp@gmail.com>
2025-01-18 19:22:00 -0300

    feat: add new interceptor to check if the auth was expired

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,4 +1,6 @@
+import { useAuthStore } from '@/store/useAuthStore'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const options = {
     baseURL: import.meta.env.VITE_API_URL
@@ -10,6 +12,19 @@ API.interceptors.response.use(
     (response) => response,
     async (error) => {
         return Promise.reject(new Error(error.response.data.error || 'Algo salió mal'))
+    }
+)
+
+API.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            const setIsAuthenticated = useAuthStore.getState().setIsAuthenticated
+            setIsAuthenticated(false)
+            const navigate = useNavigate()
+            navigate('/auth/login')
+        }
+        return Promise.reject(error)
     }
 )
 
